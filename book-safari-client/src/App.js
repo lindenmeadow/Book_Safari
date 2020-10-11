e100d4112dafc50dfc6ee9eff289bb2e275a56bf
Evan B <konros@yahoo.com>
2020-10-10 22:33:40 -0400

    Update App.js

--- book-safari-client/src/App.js
+++ book-safari-client/src/App.js
@@ -1,85 +1,42 @@
-import React, {
-  Component
-} from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-import {
-  Home
-} from './components/Home';
-import {
-  BooksPage
-} from './components/BooksPage';
-import {
-  SearchPage
-} from './components/SearchPage';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './components/Home';
+import Books from './components/Books';
+import {SearchPage} from './components/SearchPage';
 //import {Layout} from './components/Layout';
-import {
-  NavSection
-} from './components/NavSection';
-import {
-  Jumbotron
-} from './components/Banner';
-
-import {
-  Container
-} from 'react-bootstrap';
+import {NavSection} from './components/NavSection';
+import {Jumbotron} from './components/Banner';
+import {Container} from 'react-bootstrap';
 
 import './App.css';
 
 
 class App extends Component {
   render() {
-    return ( <
-      React.Fragment >
+    return ( 
+    <React.Fragment>
+      
+      <Container >
+        <div className = "content" >
+          <NavSection />
+          <Jumbotron />
 
-      <
-      Container >
-      <
-      div className = "content" >
-      <
-      NavSection / >
-      <
-      Jumbotron / >
+          <Router>
+            <Switch>
+              <Route exact path = '/' component = {Home}/> 
+              <Route exact path = '/books' component = {Books}/>
+              
+              <Route path = '/search' component = {SearchPage} /> 
+            </Switch>
 
-      <
-      Router >
-
-      <
-      Switch >
-      <
-      Route exact path = '/'
-      component = {
-        Home
-      }
-      /> <
-      Route path = '/books'
-      component = {
-        BooksPage
-      }
-      /> <
-      Route path = '/search'
-      component = {
-        SearchPage
-      }
-      /> <
-      /Switch>
-
-      <
-      /Router> <
-      /div> <
-      /Container>
-
-      <
-      /React.Fragment>
+          </Router> 
+        </div> 
+      </Container>
+      
+    </React.Fragment>
     );
   }
 
 }
 
 export default App;
-
-
-// removed Layout component--doesn't seem to be necessary
