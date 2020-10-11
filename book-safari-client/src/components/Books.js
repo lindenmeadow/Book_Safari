db96788270c45d3561bfa83c2094b71bb6532d2c
Evan B <konros@yahoo.com>
2020-10-10 22:37:06 -0400

    Update Books.js

--- book-safari-client/src/components/Books.js
+++ book-safari-client/src/components/Books.js
@@ -1,5 +1,72 @@
 import React, {Component} from 'react';
 import BookLogForm from './BookLogForm';
+import LogEntry from './LogEntry';
+import {connect} from 'react-redux';
+import {getBooks} from '../actions/BookActions'
+import {deleteBook} from '../actions/BookActions'
+//import { history } from '../index';
+
+
+class Books extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+   componentDidMount() {
+        this.props.onGet();
+    } 
+
+    render() {
+        return (
+        
+                 <div>
+                    <h1 className="log-head">Reading Log</h1>
+                    <BookLogForm />
+                    <div className="list">
+                        {
+                            this.props.books.map(book => {
+                                return (
+                                    <LogEntry
+                                        key={book.id}
+                                        book={book}
+                                        onDelete={this.props.onDelete}
+                                        
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                    
+                </div>
+            
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        books: state.books || []
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onGet: () => {
+            dispatch(getBooks());
+        },
+
+        onDelete: (id) => {
+            dispatch(deleteBook(id))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Books);
+
+
+/*
+import React, {Component} from 'react';
+import BookLogForm from './BookLogForm';
 import BookList from './BookList';
 import {connect} from 'react-redux';
 import {getBooks} from '../actions/BookLogActions'
@@ -25,3 +92,4 @@ class Books extends Component {
 }
 
 export default connect(null, {getBooks})(Books);
+*/
